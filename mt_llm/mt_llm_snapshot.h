1cd3c3f20653fa24b07be4efb197f2993a783274
Marc <RhinoDevel@users.noreply.github.com>
2025-12-17 16:23:31 +0100

    mt_llm_snapshot.h: Add comments.

--- mt_llm/mt_llm_snapshot.h
+++ mt_llm/mt_llm_snapshot.h
@@ -22,9 +22,15 @@ MT_EXPORT_LLM_API void __stdcall mt_llm_snapshot_clear(int const slot_index);
 MT_EXPORT_LLM_API bool __stdcall mt_llm_snapshot_update(int const slot_index);
 MT_EXPORT_LLM_API bool __stdcall mt_llm_snapshot_restore(int const slot_index);
 
+/**
+ * - Does NOT automatically create a snapshot, before writing to file.
+ */
 MT_EXPORT_LLM_API bool __stdcall mt_llm_snapshot_to_file(
 	int const slot_index, char const * const abs_file_path);
 
+/**
+ * - Does NOT automatically restore the snapshot, after loading from file.
+ */
 MT_EXPORT_LLM_API bool __stdcall mt_llm_snapshot_from_file(
 	char const * const abs_file_path, int const slot_index);
 
