ba956a824962e6d38c9f4b4285787e66ad55d7ba
Marc <RhinoDevel@users.noreply.github.com>
2026-03-03 19:46:25 +0100

    mt_llm_model.cpp: Add Qwen3.5-27B.

--- mt_llm/mt_llm_model.cpp
+++ mt_llm/mt_llm_model.cpp
@@ -242,6 +242,7 @@ static char const * const s_model_names_qwen3[] = {
     "Qwen3-32B",
     "Qwen3.5-35B-A3B",
     "Qwen3.5-9B",
+    "Qwen3.5-27B",
     "SmolLM3 3B", // Officially recommended: Temperature = 0.6, top-p = 0.95.
     // Add more, when necessary.
     NULL // <- DON'T FORGET THIS TERMINATING NULL ENTRY!
