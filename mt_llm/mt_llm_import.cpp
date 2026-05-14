b3d13b1d88e9b7ceec716ec695b9e9dc064a28d6
Marc <RhinoDevel@users.noreply.github.com>
2026-05-14 08:47:26 +0200

    mt_llm_import (stub).

---
+++ mt_llm/mt_llm_import.cpp
@@ -0,0 +1,12 @@
+
+// Marcel Timm, RhinoDevel, 2026may14
+
+#include <cstdbool>
+
+#include "mt_llm_import.h"
+
+MT_EXPORT_LLM_API bool __stdcall mt_llm_import(
+	char const * const messages, int const messages_cnt, int const slot_index)
+{
+	return false; // TODO: Implement!
+}
---
+++ mt_llm/mt_llm_import.h
@@ -0,0 +1,34 @@
+
+// Marcel Timm, RhinoDevel, 2026may13
+
+// This is meant to be a pure-C interface to run LLM inference.
+
+#ifndef MT_LLM_IMPORT
+#define MT_LLM_IMPORT
+
+#include "mt_llm_lib.h"
+
+#ifdef __cplusplus
+	#include <cstdbool>
+#else //__cplusplus
+	#include <stdbool.h>
+#endif //__cplusplus
+
+#ifdef __cplusplus
+extern "C" {
+#endif //__cplusplus
+
+/**
+ *  - First message given will always be interpreted as the system prompt,
+ *    second message as first user prompt, third message as first LLM response
+ *    and so on.
+ *  - Always starts import at the beginning of the context.
+ */
+MT_EXPORT_LLM_API bool __stdcall mt_llm_import(
+	char const * const messages, int const messages_cnt, int const slot_index);
+
+#ifdef __cplusplus
+}
+#endif //__cplusplus
+
+#endif //MT_LLM_IMPORT
